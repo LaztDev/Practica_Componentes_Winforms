6546c9975a939d2cdcf34a5db83f5ba8ce65fc08
Gonzalo Thomas Lazarte <gonzalo.lazt.dev@gmail.com>
2026-09-20 22:57:21 -0300

    se añadieron comentarios a ala ventana dinamica

--- Vista Dinamica -Menu/Vista Dinamica -Menu/Form1.cs
+++ Vista Dinamica -Menu/Vista Dinamica -Menu/Form1.cs
@@ -13,7 +13,12 @@ namespace Vista_Dinamica__Menu {
         }
 
         private void Menu2_Click(object sender, EventArgs e) {
-            CargarMenus(new Menu2());
+            CargarMenus(new Menu2()); // new Menu2() es una forma rapida de instanciar un objeto
+                                        // sin guardar una referencia a una variable seria lo mismo que hacer |
+            // Menu2 menu2 = new Menu(); <--------------------------------------------------------------------
+            //CargarMenu(menu2);
+            // la unica diferencia es que con esta forma comentada se guarda la referencia del objeto
+            // la mejor forma es usando sin referencia ya que no se le da el uso de un objeto como tal 
         }
 
         private void CargarMenus(UserControl vista) { // definimos que vista sera el objeto del tipo  user control
